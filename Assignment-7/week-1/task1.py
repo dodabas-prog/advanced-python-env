806df1912d788ab08eddd6ef01c5732107d1bd50
Karima Zhunis <zhunusovakarima@gmail.com>
2026-01-21 20:12:34 +0500

    aitu edition

--- Assignment-7/week-1/task1.py
+++
@@ -1,8 +0,0 @@
-f_name = input("Last name, first name? (Enter First Name): ")
-l_name = input("Last name, first name? (Enter Last Name): ")
-age_str = input("How old are you? ")
-phone_num = input("Your phone number? ")
-
-print(f"{f_name}, {l_name}")
-print(f"Your age: {age_str}")
-print(f"Your contact: {phone_num}")
